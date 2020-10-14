adc36f41e11bcd4c454e466d0bfb934e6b1bc2be
drsnails <anistu@gmail.com>
2020-10-14 16:27:42 +0300

    Initial dist subtree commit

---
+++ precache-manifest.21f51f10a3781cfe3f3163baf2d5610d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4808f543895ccfac6b33",
+    "url": "/booksDeploy/css/app.e440cf41.css"
+  },
+  {
+    "revision": "05b4a7c46f70ce467a67076731b32bd9",
+    "url": "/booksDeploy/index.html"
+  },
+  {
+    "revision": "4808f543895ccfac6b33",
+    "url": "/booksDeploy/js/app.82557303.js"
+  },
+  {
+    "revision": "8aa553b58c090ded326c",
+    "url": "/booksDeploy/js/chunk-vendors.34514bf3.js"
+  },
+  {
+    "revision": "8836d98edd5918cc97eae5f80931c809",
+    "url": "/booksDeploy/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/booksDeploy/robots.txt"
+  }
+]);
